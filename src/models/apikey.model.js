99958f55a731b878f9668b03ef8ae9364124ec6b
nguyenhoanghai@vietbank.com.vn <nguyenhoanghai0507@gmail.com>
2024-07-08 17:07:39 +0700

    Custom Dynamic Middleware for ApiKey and Permissions

---
+++ src/models/apikey.model.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const {model, Schema, Types} = require("mongoose");
+const DOCUMENT_NAME = 'ApiKey'
+const COLLECTION_NAME = 'Apikeys'
+
+const apiKeySchema = new Schema({
+    key: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    },
+    permissions: {
+        type: [String],
+        require: true,
+        enum: ['0000', '1111', '2222']
+    }
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+module.exports = model(DOCUMENT_NAME, apiKeySchema)
---
+++ src/services/apikey.service.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const apiKeyModel = require('../models/apikey.model')
+const crypto = require('crypto')
+const findById = async ( key ) =>{
+    const newKey = await apiKeyModel.create({key: crypto.randomBytes(64).toString('hex'),permissions: ['0000']})
+    console.log('newKey',newKey)
+    const objKey = await apiKeyModel.findOne({key, status: true}).lean()
+    return objKey
+}
+
+module.exports = {findById}
