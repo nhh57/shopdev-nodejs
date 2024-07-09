a711d9bd0424374300457a0c7f8fce6c05faf1e9
nguyenhoanghai@vietbank.com.vn <nguyenhoanghai0507@gmail.com>
2024-07-09 09:56:29 +0700

    Make Your API Response use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const AccessService = require('../services/access.service')
+const {CREATED} = require("../core/success.response");
 
 class AccessController {
     signUp = async (req, res, next) => {
         console.log(`[P]::signUp::`, req.body);
-        return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message: 'registered',
+            metadata: await AccessService.signUp(req.body),
+            options :{
+                limit :10
+            }
+        }).send((res))
     }
 }
 
