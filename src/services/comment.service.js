4b2e08e315c98b30fa4bb53798caf59647b91beb
nhh57 <nguyenhoanghai0507@gmail.com>
2024-08-08 11:18:50 +0700

    nested comments

---
+++ src/services/comment.service.js
@@ -0,0 +1,137 @@
+'use strict';
+
+
+/*
+    key futures: CommentService
+    + add comment [User | Shop]
+    + get a list of comment [User | Shop]
+    + delete a comment [User | Admin]
+ */
+const {convertToObjectIdMongodb} = require("../utils");
+const {NotFoundError} = require("../core/error.response");
+const {findProduct} = require("../repository/product.repo");
+
+class CommentService {
+    static async CreateCommentByProductId({productId, userId, content, parentCommentId = null}) {
+        const comment = new Comment({
+            comment_productId: productId,
+            comment_userId: userId,
+            comment_content: content,
+            comment_parentId: parentCommentId
+        })
+
+        let rightValue
+        if (parentCommentId) {
+            // reply comment
+            const parentComment = await Comment.findById(parentCommentId)
+            if (!parentComment) throw new NotFoundError(('parent comment not found'))
+            rightValue = parentComment.comment_right
+            // updateMany comments
+
+            await Comment.updateMany({
+                comment_productId: convertToObjectIdMongodb(productId),
+                comment_right: {$gte: rightValue}
+            }, {
+                $inc: {comment_right: 2}
+            })
+            await Comment.updateMany({
+                comment_productId: convertToObjectIdMongodb(productId),
+                comment_left: {$gte: rightValue}
+            }, {
+                $inc: {comment_left: 2}
+            })
+
+        } else {
+            const maxRightValue = await Comment.findOne({
+                comment_productId: convertToObjectIdMongodb(productId)
+            }, 'comment_right', {sort: {comment_right: -1}})
+            if (maxRightValue) {
+                rightValue = maxRightValue.right + 1
+            } else {
+                rightValue = 1
+            }
+        }
+
+        // insert to comment
+        comment.comment_left = rightValue;
+        comment.comment_right = rightValue + 1
+        await comment.save()
+        return comment
+    }
+
+    static async getCommentsByParentId({
+                                           productId, parentCommentId = null, limit = 50, offset = 0 // skip
+                                       }) {
+        if (parentCommentId) {
+            const parent = await Comment.findById(parentCommentId)
+            if (!parent) throw new NotFoundError('Not found comment for Product')
+            const comments = await Comment.find({
+                comment_productId: convertToObjectIdMongodb(productId),
+                comment_left: {$gt: parent.comment_left},
+                comment_right: {$lte: parent.comment_right}
+            }).select({
+                comment_left: 1,
+                comment_right: 1,
+                comment_content: 1,
+                comment_parentId: 1
+            }).sort({
+                comment_left: 1
+            })
+            return comments
+        }
+
+        const comments = await Comment.find({
+            comment_productId: convertToObjectIdMongodb(productId),
+            comment_parentId: parentCommentId
+        }).select({
+            comment_left: 1,
+            comment_right: 1,
+            comment_content: 1,
+            comment_parentId: 1
+        }).sort({
+            comment_left: 1
+        })
+        return comments
+    }
+
+    static async deleteComment({commentId, productId}) {
+        // check the product exists in the dbs
+        const foundProduct = await findProduct({
+            product_id: productId
+        })
+        if (!foundProduct) throw new NotFoundError('product not found')
+        // 1. xac dinh gia tri left va right of comment
+        const comment = await Comment.findById(commentId)
+        if (!comment) throw new NotFoundError('Comment not found')
+
+        const leftValue = comment.comment_left
+        const rightValue = comment.comment_right
+        //  2. tinh width
+        const width = rightValue - leftValue + 1
+        // 3. xoa tat ca commentId con
+        await Comment.deleteMany({
+            comment_productId: convertToObjectIdMongodb(productId),
+            comment_left: {$gte: leftValue, $lte: rightValue}
+        })
+        // 4. cao nhat gia tri left va right con lai
+        await Comment.updateMany({
+            comment_productId: convertToObjectIdMongodb(productId),
+            comment_right: {$gt: rightValue}
+        }, {
+            $inc: {comment_right: -width}
+        })
+
+        await Comment.updateMany({
+            comment_productId: convertToObjectIdMongodb(productId),
+            comment_left: {$gt: rightValue}
+        }, {
+            $inc: {comment_left: -width}
+        })
+        return true;
+    }
+
+
+}
+
+
+module.exports = CommentService
